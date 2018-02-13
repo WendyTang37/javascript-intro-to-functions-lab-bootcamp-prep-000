5e5ce39293bfa4ff26c188765b666a7f3fbc1330
Wendy Tang <wenhuitang0521@gmail.com>
2018-02-13 02:51:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,24 @@ function shout(string) {
 }
 
 function whisper(string) {
-  
+  return string.toLowerCase();
 }
+
+function logShout(string) {
+  console.log(shout(string));
+}
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+  var upperCase = shout(string);
+  if(string === upperCase) {
+    return "YES INDEED!";
+  }
+  if(string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
+
